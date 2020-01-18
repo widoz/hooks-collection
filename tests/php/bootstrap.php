94bba8b75066386371f0e7855d976f298889f952
Guido Scialfa <dev@guidoscialfa.com>
2020-01-18 19:16:22 +0100

    Update test content

--- tests/php/bootstrap.php
+++ tests/php/bootstrap.php
@@ -1,15 +1,14 @@
 <?php
-// Require Composer Auto-loader.
 require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
 
 if (!defined('WP_DEBUG')) {
     define('WP_DEBUG', true);
 }
 
-if (!defined('HOOKS_TEST_BASE_DIR')) {
-    define('HOOKS_TEST_BASE_DIR', dirname(__DIR__));
+if (!defined('TEST_BASE_DIR')) {
+    define('TEST_BASE_DIR', dirname(__DIR__));
 }
 
-if (!defined('HOOKS_PROJECT_BASE_DIR')) {
-    define('HOOKS_PROJECT_BASE_DIR', dirname(__DIR__, 2));
+if (!defined('PROJECT_BASE_DIR')) {
+    define('PROJECT_BASE_DIR', dirname(__DIR__, 2));
 }
